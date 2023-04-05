59e9f63280150d3e77b7faa850a0adecc4c1a8b8
Emeric Laberge <emericlaberge@gmail.com>
2023-04-05 18:15:49 -0400

    inital commit

---
+++ lua/Emeric/core/options.lua
@@ -0,0 +1,43 @@
+local opt = vim.opt -- for conciseness
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+vim.api.nvim_set_hl(0, 'LineNr', { fg =DarkTurquoise } )
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+opt.wrap =true -- 
+opt.linebreak = true -- wrap on words
+-- make the line wrap at the same column as the text width 
+opt.textwidth = 80 -- wrap at 80 characters
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+
+opt.colorcolumn = "80" -- show a vertical line at column 80
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.termguicolors = true
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+opt.iskeyword:append("-") -- consider string-string as whole word
+
