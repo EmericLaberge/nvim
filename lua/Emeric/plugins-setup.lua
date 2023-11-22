277ad792ec42a0cef55c7d79993deba4591cd7a5
EmericLaberge <102610334+EmericLaberge@users.noreply.github.com>
2023-11-21 22:19:09 -0500

    21nov

--- lua/Emeric/plugins-setup.lua
+++ lua/Emeric/plugins-setup.lua
@@ -257,6 +257,19 @@ return packer.startup(function(use)
     end
   }
 
+-- which key
+use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
   -- neorg for note taking
   use {
     "nvim-neorg/neorg",
