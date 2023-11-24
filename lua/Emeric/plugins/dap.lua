507076ecdf7a9974427abadd478a7104db7a779d
EmericLaberge <102610334+EmericLaberge@users.noreply.github.com>
2023-11-24 09:34:31 -0500

    Revert "dap"
    This reverts commit eb2b1191a28620c27eddd1f3163fdce2489a5ad9.

--- lua/Emeric/plugins/dap.lua
+++ lua/Emeric/plugins/dap.lua
@@ -1,68 +1,34 @@
+-- lua/Emeric/plugins/dap.lua
 local dap = require('dap')
 local dapui = require('dapui')
-require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
--- Configure DAP UI
-dapui.setup({
-  -- Here you can configure the layout and elements of DAP UI
-  -- This is a basic setup with two windows: a sidebar and a tray at the bottom
-  layouts = {
-    {
-      elements = {
-        -- Elements can be 'scopes', 'breakpoints', 'stacks', 'watches', 'repl'
-        { id = 'scopes', size = 0.25 },
-        'breakpoints',
-        { id = 'stacks', size = 0.25 },
-        'watches',
-      },
-      size = 40, -- The width of the sidebar
-      position = 'left', -- The side of the screen it's on
-    },
-    {
-      elements = {
-        'repl',
-        'console',
-      },
-      size = 10, -- The height of the tray
-      position = 'bottom', -- The bottom of the screen
-    },
-  },
-  -- Other configurations like floating window settings and keymaps
-})
 
--- Automatically open the DAP UI when starting a debug session
-dap.listeners.after.event_initialized['dapui_config'] = function()
-  dapui.open()
-end
+-- Configure debuggers here
+-- For example, here's a configuration for a Python debugger
+dap.adapters.python = {
+  type = 'executable';
+  command = '/usr/bin/python3'; -- Replace with the path to your Python executable
+  args = { '-m', 'debugpy.adapter' };
+}
 
--- Automatically close the DAP UI when the debug session is terminated
-dap.listeners.before.event_terminated['dapui_config'] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited['dapui_config'] = function()
-  dapui.close()
-end
-
--- Add your debug configurations for different languages below
--- For example, here is a simple configuration for Python using debugpy
 dap.configurations.python = {
   {
-    type = 'python', -- the type here must match the name of the dap adapter
-    request = 'launch',
+    -- The name of the configuration
     name = "Launch file",
-    program = "${file}", -- This will use the file in the current buffer
+    type = 'python', -- The type here should match the adapter type
+    request = 'launch',
+    program = "${file}", -- This will start the file you are currently editing
     pythonPath = function()
-      -- Path to the python executable. Adjust as needed.
-      return '/usr/bin/python3'
+      -- Update this path to match the path to your Python interpreter
+      return '/usr/bin/python'
     end,
   },
 }
--- You would add similar configurations for other languages you are working with
 
--- Key mappings for common DAP operations can go here
-vim.api.nvim_set_keymap('n', '<F5>', '<Cmd>lua require"dap".continue()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F10>', '<Cmd>lua require"dap".step_over()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F11>', '<Cmd>lua require"dap".step_into()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F12>', '<Cmd>lua require"dap".step_out()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>b', '<Cmd>lua require"dap".toggle_breakpoint()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>B', '<Cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>lp', '<Cmd>lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', { noremap = true })
+-- Other language configurations go here
+
+-- Optionally, you can also set up mappings for `nvim-dap` commands
+vim.fn.nvim_set_keymap('n', '<F5>', '<Cmd>lua require("dap").continue()<CR>', { noremap = true, silent = true })
+-- Add more mappings as needed
+
+return dap
+
--- lua/Emeric/plugins/lsp.lua
+++ lua/Emeric/plugins/lsp.lua
@@ -1,16 +1,14 @@
 local lsp = require("lsp-zero")
-local lspconfig = require("lspconfig")
-
 lsp.preset("recommended")
 
 lsp.ensure_installed({
   'tsserver',
   'rust_analyzer',
-  -- 'jdtls', -- This line is removed or commented out to remove jdtls
-  -- other LSP servers
+
 })
 
--- Configure Lua LSP server
+
+-- Fix Undefined global 'vim'
 lsp.configure('lua-language-server', {
     settings = {
         Lua = {
@@ -21,30 +19,51 @@ lsp.configure('lua-language-server', {
     }
 })
 
--- ... other LSP server configurations for Python, etc ...
 
--- CMP configuration
--- ... (rest of your cmp configuration)
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
 
--- Set general LSP preferences
--- ... (rest of your lsp preferences)
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
 
--- General on_attach function for LSP servers
--- ... (rest of your on_attach configuration)
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
 
--- Apply the LSP settings
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
 lsp.setup()
 
--- Diagnostic virtual text configuration
 vim.diagnostic.config({
     virtual_text = true,
 })
-
--- You can remove the autocommand for jdtls if you no longer need it
--- vim.api.nvim_create_autocmd('FileType', {
---   pattern = 'java',
---   callback = function()
---     -- Your previous jdtls setup would have been here
---   end,
--- })
 
