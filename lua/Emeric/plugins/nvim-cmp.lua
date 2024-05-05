e45c4f18740f0811a2c7a0310ea8e911d372580e
EmericLaberge <102610334+EmericLaberge@users.noreply.github.com>
2024-05-05 01:40:23 -0400

    cmp priority

--- lua/Emeric/plugins/nvim-cmp.lua
+++ lua/Emeric/plugins/nvim-cmp.lua
@@ -32,16 +32,16 @@ cmp.setup({
     ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+    ["<C-Space>"] = cmp.mapping.complete(),            -- show completion suggestions
+    ["<C-e>"] = cmp.mapping.abort(),                   -- close completion window
     ["<CR>"] = cmp.mapping.confirm({ select = true }), -- confirm completion
   }),
   -- sources for autocompletion
   sources = cmp.config.sources({
-    { name = "nvim_lsp" }, -- lsp
-    { name = "luasnip" }, -- snippets
-    { name = "buffer" }, -- text within current buffer
-    { name = "path" }, -- file system paths
+    { name = "nvim_lsp",  group_index = 1 }, -- LSP
+    { name = "luasnip",   group_index = 1 },
+    { name = "buffer", group_index = 2 }, -- buffer
+    { name = "path", group_index = 3 }, -- file system paths
   }),
   -- configure lspkind for vs-code like icons
   formatting = {
