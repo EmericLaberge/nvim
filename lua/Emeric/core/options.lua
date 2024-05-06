5839d7bac1277e4cac60c561612da12963b6bda8
EmericLaberge <102610334+EmericLaberge@users.noreply.github.com>
2024-05-06 16:00:35 -0400

    Tokyo + options

--- lua/Emeric/core/options.lua
+++ lua/Emeric/core/options.lua
@@ -3,7 +3,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
-vim.api.nvim_set_hl(0, 'LineNr', { fg =DarkTurquoise } )
+
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
