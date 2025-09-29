24a7b371037e5b0d2bf8ec03874137847421a889
EmericLaberge <102610334+EmericLaberge@users.noreply.github.com>
2025-09-29 16:30:52 -0400

    hop fixed

--- lua/Emeric/plugins/hop.lua
+++ lua/Emeric/plugins/hop.lua
@@ -3,6 +3,9 @@ return {
   branch = "v2",
   config = function()
     local hop = require('hop')
+    -- Ensure Hop is initialized before using its API
+    hop.setup({ keys = 'etovxqpdygfblzhckisuran' })
+
     local directions = require('hop.hint').HintDirection
     vim.keymap.set('', 'f', function()
       hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
